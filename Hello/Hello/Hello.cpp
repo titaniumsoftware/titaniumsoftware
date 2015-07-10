161a37ae311cd89acbd06b1be93fe605aad0cbff
titanium software <titanium.software.uk@hotmail.com>
2015-07-10 23:19:40 +0100

    created hello project

---
+++ Hello/Hello/Hello.cpp
@@ -0,0 +1,15 @@
+// Hello.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    cout << "Hello\n";
+
+	return 0;
+}
+
